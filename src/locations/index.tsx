106222f692e081dc1a7f4dea9c8125d2cb982c4b
arjun s kumar <arjun@simplogics.com>
2025-04-16 15:25:24 +0530

    install router v6 and pages

---
+++ src/locations/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+// import { ErrorBoundary } from "../plugins/error-boundary";
+import { Locations } from "../constants/locations";
+import  Login  from "../pages/login";
+
+export const webRouter = createBrowserRouter([
+	{
+		path: Locations.LOGIN,
+		element: <Login />,
+		// errorElement: <ErrorBoundary />,
+	},
+	{
+		// path: Locations.BASE,
+		// element: (
+		// 	<MainContainer />
+		// ),
+		// errorElement: <ErrorBoundary />,
+		// children: [
+		// ],
+	},
+	{
+		path: "*",
+		element: <Navigate to={Locations.BASE} />,
+		// errorElement: <ErrorBoundary />,
+	},
+]);
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,11 @@
+import React, { JSX } from 'react'
+
+const Login = (): JSX.Element => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Login
